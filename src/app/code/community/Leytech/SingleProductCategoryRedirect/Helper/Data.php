1870c659deaf2be8309e51235a6a0e3c95380600
Chris Nolan <chris@leytech.co.uk>
2017-04-27 20:26:45 +0100

    License and copyright changes

--- src/app/code/community/Leytech/SingleProductCategoryRedirect/Helper/Data.php
+++ src/app/code/community/Leytech/SingleProductCategoryRedirect/Helper/Data.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @package    Leytech_SingleProductCategoryRedirect
+ * @author     Chris Nolan (chris@leytech.co.uk)
+ * @copyright  Copyright (c) 2017 Leytech
+ * @license    https://opensource.org/licenses/MIT  The MIT License  (MIT)
+ */
+
 class Leytech_SingleProductCategoryRedirect_Helper_Data extends Mage_Core_Helper_Abstract
 {
+
 }
--- src/app/code/community/Leytech/SingleProductCategoryRedirect/Model/Observer.php
+++ src/app/code/community/Leytech/SingleProductCategoryRedirect/Model/Observer.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package    Leytech_SingleProductCategoryRedirect
+ * @author     Chris Nolan (chris@leytech.co.uk)
+ * @copyright  Copyright (c) 2017 Leytech
+ * @license    https://opensource.org/licenses/MIT  The MIT License  (MIT)
+ */
+
 class Leytech_SingleProductCategoryRedirect_Model_Observer
 {
     public function SingleProductCategoryRedirect($observer)
