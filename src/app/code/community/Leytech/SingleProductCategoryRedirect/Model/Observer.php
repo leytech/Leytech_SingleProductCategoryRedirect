4e92e8183a3fedeeef35dbcbd8121ad81de5c0b9
Chris Nolan <chris@leytech.co.uk>
2017-05-01 12:30:01 +0100

    Helper function and little tidy up

--- src/app/code/community/Leytech/SingleProductCategoryRedirect/Model/Observer.php
+++ src/app/code/community/Leytech/SingleProductCategoryRedirect/Model/Observer.php
@@ -10,11 +10,13 @@ class Leytech_SingleProductCategoryRedirect_Model_Observer
 {
     public function SingleProductCategoryRedirect($observer)
     {
-        if (Mage::getStoreConfig("leytech_singleproductcategoryredirect/settings/enable_ext")) {
+        $helper = Mage::helper('leytech_singleproductcategoryredirect');
+
+        if ($helper->isEnabled()) {
             $_category = $observer->getEvent()->getCategory();
             $products_count = $_category->getProductCollection()->count();
 
-            if ($products_count==1){
+            if ($products_count == 1) {
                 $url = $_category->getProductCollection()->getFirstItem()->getProductUrl();
                 return $observer->getEvent()->getControllerAction()->getResponse()->setRedirect($url);
             }
